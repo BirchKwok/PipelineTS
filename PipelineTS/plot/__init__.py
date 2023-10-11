1b459400c1917a7824e5c27a88e764ccc6f9ced7
birchkwok <birchkwok@gmail.com>
2023-10-11 11:40:57 +0800

    新增区间绘图功能

--- PipelineTS/plot/__init__.py
+++ PipelineTS/plot/__init__.py
@@ -4,33 +4,51 @@ from spinesUtils.asserts import ParameterTypeAssert, ParameterValuesAssert
 
 
 @ParameterTypeAssert({
-    'data': pd.DataFrame,
-    'test_data': pd.DataFrame,
+    'data1': pd.DataFrame,
+    'data2': pd.DataFrame,
     'time_col': str,
     'target_col': str,
     'labels': (None, list, tuple)
 })
-@ParameterValuesAssert({
-    'labels': lambda s: (s is None) or (len(s) == 2 and all(isinstance(i, str) for i in s))
-})
-def plot_data_period(data, test_data, time_col, target_col, labels=None):
+def plot_data_period(data1, data2, time_col, target_col, labels=None):
     """可视化日期数据"""
     import matplotlib.pyplot as plt
     import matplotlib.dates as m_dates
 
-    if labels is None:
-        labels = ('Data', 'Test Data')
+    assert data1[time_col].iloc[0] <= data2[time_col].iloc[0], \
+        'The starting time of data1 must be before data2, or equal to the starting time of data2.'
+
+    if f'{target_col}_upper' in data2.columns and f'{target_col}_lower' in data2.columns:
+        if labels is None:
+            labels = ('True Data', 'Prediction Data', 'Upper Bound', 'Lower Bound')
+        else:
+            assert len(labels) == 4
+    else:
+        if labels is None:
+            labels = ('Data', 'Test Data')
+        else:
+            assert len(labels) == 2
+
     # 绘制曲线
-    plt.plot_date(data[time_col], data[target_col], label=labels[0],
+    plt.plot_date(data1[time_col], data1[target_col], label=labels[0],
                   color='black', linestyle='-', marker='')
-    plt.plot_date(test_data[time_col], test_data[target_col],
+    plt.plot_date(data2[time_col], data2[target_col],
                   label=labels[1], color='blue', linestyle='-', marker='')
 
-    # 添加竖线
-    plt.axvline(x=test_data[time_col].iloc[0], color='black', linestyle='--')
+    if f'{target_col}_upper' in data2.columns and  f'{target_col}_lower' in data2.columns:
+        plt.plot_date(data2[time_col], data2[f'{target_col}_upper'],
+                      label=labels[2], color='orchid', linestyle='-', marker='')
+        plt.plot_date(data2[time_col], data2[f'{target_col}_lower'],
+                      label=labels[3], color='violet', linestyle='-', marker='')
+        plt.fill_between(data2[time_col], data2[f'{target_col}_upper'], data2[f'{target_col}_lower'],
+                         color='dimgray', alpha=0.2)
 
-    valid_data_max = test_data[target_col].max()
-    valid_period = [test_data[time_col].iloc[0], test_data[time_col].iloc[-1]]
+    if data2[time_col].iloc[0] > data1[time_col].iloc[0]:
+        # 添加竖线
+        plt.axvline(x=data2[time_col].iloc[0], color='black', linestyle='--')
+
+    valid_data_max = data2[target_col].max()
+    valid_period = [data2[time_col].iloc[0], data2[time_col].iloc[-1]]
 
     plt.axvspan(valid_period[0], valid_period[1], ymin=0, ymax=valid_data_max,
                 facecolor='lightyellow', alpha=0.5)
