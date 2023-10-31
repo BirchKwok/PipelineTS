eb973d8b293660875e69ba705d7fd386a6905de4
birchkwok <birchkwok@gmail.com>
2023-10-31 17:58:29 +0800

    New feature: Ability to define custom names for models in PipelineConfigs class

--- PipelineTS/pipeline/pipeline_configs.py
+++ PipelineTS/pipeline/pipeline_configs.py
@@ -14,12 +14,24 @@ from PipelineTS.pipeline.pipeline_models import get_all_available_models
 MODELS = get_all_available_models()
 
 
+def configs_check(configs):
+    condition_a = all(
+        isinstance(i, tuple) and 2 <= len(i) <= 3 and isinstance(i[-1], dict) for i in configs
+    )
+
+    condition_b = all(
+       isinstance(i[0], str) if len(i) == 2 else isinstance(i[0], str) and isinstance(i[1], str) for i in configs
+    )
+
+    return condition_a and condition_b
+
+
 class PipelineConfigs:
     @ParameterTypeAssert({
         'configs': list
     })
     @ParameterValuesAssert({
-        'configs': lambda s: all(isinstance(i, tuple) and len(i) == 2 and isinstance(i[1], dict) for i in s)
+        'configs': configs_check
     })
     def __init__(self, configs):
         self.sub_configs = {'init_configs': {}, 'fit_configs': {}, 'predict_configs': {}}
@@ -28,7 +40,13 @@ class PipelineConfigs:
 
         self.configs = []
         for models_group in reindex_iterable_object(_to_process_configs, key=lambda s: s[0], index_start=1):
-            for (index, (model_name, model_configs)) in models_group:
+            for (index, (*model_name_list, model_configs)) in models_group:
+                model_name = model_name_list[0]
+                if len(model_name_list) == 2:
+                    model_name_after_rename = model_name_list[1]
+                else:
+                    model_name_after_rename = f"{model_name}_{index}"
+
                 if not all(i in self.sub_configs for i in model_configs):
                     raise KeyError
 
@@ -39,20 +57,20 @@ class PipelineConfigs:
                     model_configs.update({k: self.sub_configs[k]
                                           for k in self.sub_configs if k not in model_configs})
 
-                self.configs.append((model_name, f"{model_name}_{index}", model_configs))
+                self.configs.append((model_name, model_name_after_rename, model_configs))
 
         if is_in_ipython():
             display(
-                tabulate(self.configs, headers=['model_name', 'model_name_with_index', 'model_configs'],
+                tabulate(self.configs, headers=['model_name', 'model_name_after_rename', 'model_configs'],
                          tablefmt='html', colalign=("right", "left", "left"), showindex='always')
             )
         else:
             print(
-                tabulate(self.configs, headers=['model_name', 'model_name_with_index', 'model_configs'],
+                tabulate(self.configs, headers=['model_name', 'model_name_after_rename', 'model_configs'],
                          tablefmt='pretty', colalign=("right", "left", "left"), showindex='always')
             )
 
     def get_configs(self, model_name_after_rename):
-        for (model_name, mnwi, model_configs) in self.configs:
-            if model_name_after_rename == mnwi:
+        for (model_name, mnar, model_configs) in self.configs:
+            if model_name_after_rename == mnar:
                 return model_configs
