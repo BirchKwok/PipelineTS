af665a4e2f284720298e3fb17d5b2eeb99fa1779
birchkwok <birchkwok@gmail.com>
2023-11-10 17:13:04 +0800

    Added utility function set file

---
+++ PipelineTS/utils/__init__.py
@@ -0,0 +1,31 @@
+# utility functions
+
+def load_dataset_to_darts(
+        data,
+        time_col,
+        target_col,
+        fill_missing_dates=False,
+        freq=None,
+        fillna_value=None,
+        static_covariates=None,
+        hierarchy=None
+):
+    from darts.timeseries import TimeSeries
+
+    return TimeSeries.from_dataframe(
+        data,
+        time_col=time_col,
+        value_cols=target_col,
+        fill_missing_dates=fill_missing_dates,
+        freq=freq,
+        fillna_value=fillna_value,
+        static_covariates=static_covariates,
+        hierarchy=hierarchy
+    )
+
+
+def update_dict_without_conflict(dict_a, dict_b):
+    for i in dict_b:
+        if i not in dict_a:
+            dict_a[i] = dict_b[i]
+    return dict_a
