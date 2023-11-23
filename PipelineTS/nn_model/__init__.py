62a6a8ba09a7b8d70130c13c87ce7322740c824c
birchkwok <birchkwok@gmail.com>
2023-11-23 14:02:45 +0800

    Rename model

--- PipelineTS/nn_model/__init__.py
+++ PipelineTS/nn_model/__init__.py
@@ -4,9 +4,9 @@ from PipelineTS.nn_model.n_beats import NBeatsModel
 from PipelineTS.nn_model.n_hits import NHitsModel
 from PipelineTS.nn_model.tft import TFTModel
 from PipelineTS.nn_model.gau import GAUModel
-from PipelineTS.nn_model.rnn import StackingRNNModel
+from PipelineTS.nn_model.stacking_rnn import StackingRNNModel
 from PipelineTS.nn_model.time2vec import Time2VecModel
 from PipelineTS.nn_model.transformer import TransformerModel
 from PipelineTS.nn_model.tide import TiDEModel
-from PipelineTS.nn_model.seg_rnn import SegRNNModel
+from PipelineTS.nn_model.patch_rnn import PatchRNNModel
 from PipelineTS.nn_model.tcn import TCNModel
